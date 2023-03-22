a93ffa3279f7265776f0ba14c2f63099ac168a68
Raphael brun <rb@triamec.com>
2023-03-22 10:32:19 +0100

    Add project files.

---
+++ Hello World!/Program.cs
@@ -0,0 +1,20 @@
+// Copyright © 2007 Triamec Motion AG
+
+using System;
+using System.Windows.Forms;
+
+namespace Triamec.Tam.Samples {
+	using static Application;
+
+	static class Program {
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() {
+			EnableVisualStyles();
+			SetCompatibleTextRenderingDefault(false);
+			Run(new HelloWorldForm());
+		}
+	}
+}
